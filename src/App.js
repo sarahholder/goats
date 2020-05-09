1ec48f2c0389aeadf0e9adfc6095e5eb7201e3f8
Sarah Holder <sr.holder@yahoo.com>
2020-05-09 13:33:31 -0500

    corrected react app function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <button className="btn btn-dark">Bootstrap Button</button>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+        <h1>GOATS</h1>
+        <button className="btn btn-dark">Bootstrap Button</button>
+        </div>
+    );
+  }
 }
-
 export default App;
